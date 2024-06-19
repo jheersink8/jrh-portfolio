9889bf9737b2440ee8c940eb346ae6d3e0a51d1c
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-06-18 22:19:18 -0600

    More nav function added.

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -2,7 +2,7 @@
 export default function Intro() {
     return (
         <div>
-            <img src='../../images/headshot.PNG' />
+            <img src='src\components\Intro\Images\headshot.png' />
             <h1>Hello. My name is Jordan Heersink. Thank you so much for visiting my site! Please scroll down to learn more about me, and use the navigation buttons to access more of my web development resources.</h1>
         </div>
     )
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,45 +1,54 @@
 import { DarkModeIcon } from "./Icons/Dark-mode";
 import { LightModeIcon } from "./Icons/Light-mode";
 import { MuteIcon } from "./Icons/Mute";
-// import { UnmuteIcon } from "./Icons/Unmute";
+import { UnmuteIcon } from "./Icons/Unmute";
 
 export default function Nav() {
     return (
-        <nav className="navbar navbar-expand-lg">
+        <nav className="navbar sticky-top navbar-expand-lg">
             <div className="container-fluid">
 
                 {/* Button to reload page */}
-                <a class="navbar-brand" href="/">Jordan R. Heersink</a>
+                <a className="navbar-brand ms-4" href="/">Jordan R. Heersink</a>
 
                 {/* Hamburger button for smaller viewport */}
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
 
                 {/* Navigation links */}
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link" href="#about">About</a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav m-auto mb-lg-0">
+
+                        <li className="nav-item mx-4">
+                            <a className="nav-link active" href="#about">About</a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#projects">Projects</a>
+
+                        <li className="nav-item mx-4">
+                            <a className="nav-link active" href="#projects">Projects</a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#skills">Skills</a>
+
+                        <li className="nav-item mx-4">
+                            <a className="nav-link active" href="#skills">Skills</a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#resume">Resume</a>
+
+                        <li className="nav-item mx-4">
+                            <a className="nav-link active" href="#resume">Resume</a>
                         </li>
+
                     </ul>
 
                     {/* Dark-mode/Light-mode */}
-                    <div>  <DarkModeIcon fill="#000000" width="24" height="24" /></div>
-                    <div>  <LightModeIcon fill="#000000" width="36" height="36" /></div>
-
+                    <div className="me-0">
+                        <DarkModeIcon fill="#000000" width="24" height="24" />
+                        <LightModeIcon fill="#000000" width="36" height="36" />
+                    </div>
+                    <div className="mx-3">|</div>
                     {/* Mute/Unmute */}
-                    <div>  <MuteIcon fill="#000000" width="36" height="36" /></div>
-
+                    <div className="me-4">
+                        <UnmuteIcon fill="#000000" width="36" height="36" />
+                        <MuteIcon fill="#000000" width="38" height="38" />
+                    </div>
 
                 </div>
             </div>
